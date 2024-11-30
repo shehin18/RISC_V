639619af90e64a525ec4e188d3f5852f4dd74f64
Muhammed Shehin <92203325+shehin18@users.noreply.github.com>
2024-11-30 10:37:20 +0530

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -8,31 +8,31 @@ output reg [31:0] ALU_result;
 always @(*) begin
     case (ALU_control)
         4'b0000: begin
-        Zero <= 0;
-        ALU_result <= A & B;
+        Zero = 0;
+        ALU_result = A & B;
         end
        
         4'b0001: begin
-        Zero <= 0;
-        ALU_result <= A | B;
+        Zero = 0;
+        ALU_result = A | B;
         end
 
         4'b0010: begin
-        Zero <= 0;
-        ALU_result <= A + B;
+        Zero = 0;
+        ALU_result = A + B;
         end
 
         4'b0110: begin
             if(A == B)
-            Zero <= 1;
+            Zero = 1;
             else begin
-            Zero <= 0;
-            ALU_result <= A - B; end     
+            Zero = 0;
+            ALU_result = A - B; end     
         end
 
         default: begin
-        Zero <= 0;
-        ALU_result <= A;
+        Zero = 0;
+        ALU_result = A;
         end
     endcase
 end
